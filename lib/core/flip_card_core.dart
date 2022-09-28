579a9968e85251203ac50d6e464bf69c4db07f8a
hoigas <0906sm@gmail.com>
2022-09-28 16:23:02 +0900

    [2주차] 1단계 UI-로직 분리 (#2)
    * [2주차] 카드 게임 UI적용
    
    * [2주차] 1단계 UI-로직 분리, FlipCardCore class로 분리
    
    * [2주차] 1단계 UI-로직 분리, StreamContoller 사용 listen사용
    
    * [2주차] 1단계 UI-로직 분리, body를 streamBuider를 사용하여 수정
    
    [2주차] 1단계 UI-로직 분리, body를 streamBuider를 전달하는 방식 만드는 중
    
    [2주차] 1단계 UI-로직 분리, body를 streamBuider를 사용하여 수정
    
    * [2주차] 1단계 UI-로직 분리, streamBuider에서 이벤트를 전달방식 수정
    
    [2주차] 1단계 UI-로직 분리, streamBuider에서 이벤트를 전달방식 수정
    
    Co-authored-by: 한성민 <seongmin.han@cjconnect.io>

---
+++ lib/core/flip_card_core.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+
+import 'package:flip_card/flip_card.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../screen/asset_image_name.dart';
+
+enum FlipCardCoreEvent { resetCard, equalCard }
+
+class FlipCardCore {
+  final _imageNames = [
+    AssetImageName.orange,
+    AssetImageName.banana,
+    AssetImageName.apple,
+    AssetImageName.strawberry,
+  ];
+
+  List<String> _randomImageNames = [];
+
+  List<GlobalKey<FlipCardState>> _cardKeys = [];
+
+  List<GlobalKey<FlipCardState>> get cardKeys => _cardKeys;
+  int _frontCardCount = 0;
+  final List<int> _frontCardIndexes = [];
+
+  List<String> get randomImageNames => _randomImageNames;
+
+  int get frontCardCount => _frontCardCount;
+
+  List<int> get frontCardIndexes => _frontCardIndexes;
+
+  final StreamController<FlipCardCoreEvent> _streamController = StreamController();
+
+  Stream<FlipCardCoreEvent>? get stream => _streamController.stream;
+
+  void increaseFrontCardCount() {
+    _frontCardCount++;
+  }
+
+  void toggleCardToFront() {
+    for (var cardKey in _cardKeys) {
+      if (!(cardKey.currentState?.isFront ?? false)) {
+        cardKey.currentState?.toggleCard();
+      }
+    }
+  }
+
+  void reset() {
+    // add 2 times
+    _randomImageNames.clear();
+    _randomImageNames.addAll(_imageNames);
+    _randomImageNames.addAll(_imageNames);
+
+    // shuffle
+    _randomImageNames.shuffle();
+
+    // create global key
+    _frontCardCount = 0;
+    _cardKeys.clear();
+    _cardKeys.addAll(_randomImageNames.map((_) => GlobalKey<FlipCardState>()));
+
+    _streamController.add(FlipCardCoreEvent.resetCard);
+  }
+
+  void checkCardIsEqual() {
+    if (_frontCardIndexes.length >= 2) {
+      String firstCardName = _randomImageNames[_frontCardIndexes[0]];
+      String secondCardName = _randomImageNames[_frontCardIndexes[1]];
+      if (firstCardName == secondCardName) {
+        _randomImageNames[_frontCardIndexes[0]] = '';
+        _randomImageNames[_frontCardIndexes[1]] = '';
+
+        _streamController.add(FlipCardCoreEvent.equalCard);
+      }
+    }
+
+    _frontCardIndexes.clear();
+    _frontCardCount = 0;
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flutter/material.dart';
+
+import '../core/flip_card_core.dart';
+
+class HomeScreen extends StatefulWidget {
+  final String title;
+
+  const HomeScreen({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  FlipCardCore flipCardCore = FlipCardCore();
+
+  @override
+  void initState() {
+    super.initState();
+    flipCardCore.reset();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: StreamBuilder<FlipCardCoreEvent>(
+          stream: flipCardCore.stream,
+          builder: (context, snapshot) {
+            print("AAAA ${snapshot.data}");
+            if (snapshot.data == null) {
+              return Container();
+            } else if (snapshot.data == FlipCardCoreEvent.resetCard || snapshot.data == FlipCardCoreEvent.equalCard) {
+              return Center(
+                child: Wrap(
+                  spacing: 4,
+                  runSpacing: 4,
+                  children: List.generate(
+                    flipCardCore.randomImageNames.length,
+                    (index) {
+                      if (flipCardCore.randomImageNames[index].isEmpty) {
+                        return Container(
+                          width: 90,
+                          height: 150,
+                          color: Colors.transparent,
+                        );
+                      }
+                      return FlipCard(
+                        key: flipCardCore.cardKeys[index],
+                        onFlip: () {
+                          flipCardCore.increaseFrontCardCount();
+                          flipCardCore.frontCardIndexes.add(index);
+                        },
+                        onFlipDone: (bool) {
+                          if (flipCardCore.frontCardCount == 2) {
+                            flipCardCore.toggleCardToFront();
+                            flipCardCore.checkCardIsEqual();
+                          }
+                        },
+                        front: Container(
+                          width: 90,
+                          height: 150,
+                          color: Colors.orange,
+                        ),
+                        back: Container(
+                          width: 90,
+                          height: 150,
+                          child: Image.asset(
+                            flipCardCore.randomImageNames[index],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              );
+            }
+            return Container();
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          flipCardCore.reset();
+        },
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
